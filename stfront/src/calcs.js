47be64cf90e41546df62a0023510bdcbc203ba18
Sheel Sansare <sheels00@gmail.com>
2022-07-02 22:48:23 -0700

    added inputs

---
+++ stfront/src/calcs.js
@@ -0,0 +1,34 @@
+///////////////////INPUTS////////////////////////
+//loading input
+const P_dl = 0;
+const P_ll = 0;
+const w_overburden = 0;
+//footing paramter input 
+const q_allow = 0;
+const include_ftg_sw = false;
+//concrete 
+const X_ftg = 0;
+const Y_ftg = 0;
+const X_col = 0;
+const Y_col = 0;
+const f_c = 0;
+const cover = 0;
+const h = 0;
+//flexure reinforcement x-dir
+const x_bar_size = 0;
+const x_number_bars = 0;
+const x_f_y = 0;
+//flexure reinforcement y-dir
+const y_bar_size = 0;
+const y_number_bars = 0;
+const y_f_y = 0;
+//shear reinforcement (optional)
+const shear_f_yt = 0;
+const shear_bar_size = 0;
+const spacing_x = 0;
+const spacing_y = 0;
+//size effect factor
+const include = false;
+
+
+///////////////////calcs////////////////////////
