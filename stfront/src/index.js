904a36a1f3cd9ad0017eb28e6b291c3f11b2022d
DeepakMoparthi123 <deepakm3@illinois.edu>
2022-07-03 01:19:51 -0500

    basic accordion + input

--- stfront/src/index.js
+++ stfront/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container } from "semantic-ui-react";
+import reportWebVitals from './reportWebVitals';
+import Example from "./example";
+
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+
+styleLink.href =
+    "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+
+var inputs = ['P_dl', 'P_ll', 'w_overburden', 'q_allow',
+    'include_ftg_sw', 'X_ftg', 'Y_ftg', 'X_col', 'Y_col', 'f_c',
+    'cover', 'h', 'x_bar_size', 'x_number_bars', 'x_f_y',
+    'y_bar_size', 'y_number_bars', 'y_f_y', 'shear_f_yt', 'shear_bar_size',
+    'spacing_x', 'spacing_y', 'include']
+
+function MyForm() {
+    console.log(inputs)
+    return (
+        <form>
+            {inputs.map(function (object, i) {
+                return (<label>{object}
+                    <input type="text" />
+                </label>);
+            })}
+
+        </form>
+    )
+}
+
+
+ReactDOM.render(
+
+    <Container style={{ margin: 20 }}>
+        <MyForm />
+        <Example />
+    </Container>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
