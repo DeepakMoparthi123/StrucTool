9016bf57cd9d8b0eacbed52aa838a7e7f0c4d8b0
Sheel Sansare <sheels00@gmail.com>
2022-07-03 21:31:06 -0700

    completed flexure calcs

--- stfront/src/calcs.js
+++ stfront/src/calcs.js
@@ -48,25 +48,42 @@ const lamda_s = 1;
 
 ///////////////////calcs////////////////////////
 const d = h - cover - Math.max(getDia(x_bar_size), getDia(y_bar_size)) - Math.min(getDia(x_bar_size), getDia(y_bar_size)) / 2;
-//flexure about y-axis
-const x_A_st_provided = getArea(x_bar_size) * x_number_bars;
-//flexure about x-axis
-const y_A_st_provided = getArea(y_bar_size) * y_number_bars;
-//one way shear
 const P_sw = X_ftg/12 * Y_ftg/12 * h/12 * 0.15;
 const P_service_net = P_dl + P_ll;
 const P_u_net = Math.max(1.2*P_dl + 1.6*P_ll, 1.4*P_dl);
 const q_u_net = P_u_net / (X_ftg/12 * Y_ftg/12);
+//flexure
+const beta = f_c < 4 ? 0.85 : f_c > 8 ? 0.65 : 1.05 - .05*f_c;
+const rho_min = .0018
+    //about y-axis
+const A_st_provided_y = getArea(x_bar_size) * x_number_bars;
+const a_y = A_st_provided_y * x_f_y / .85 / Y_ftg / f_c;
+const c_y = a_y / beta;
+const epsilon_t_y = (d - c_y) / c_y * .003;
+const phi_b_y = epsilon_t_y < .002 ? .65 : epsilon_t_y > .005 ? .9 : .65 + (epsilon_t_y - .002) * 250/3;
+const phiMn_y = phi_b_y * A_st_provided_y * x_f_y * (d - a_y/2);
+const M_u_y = q_u_net * (X_ftg/12/2 - X_col/12/2)**2 / 2 * Y_ftg;
+const flexure_y_DCR = M_u_y / phiMn_y;
+    //about x-axis
+const A_st_provided_x = getArea(y_bar_size) * y_number_bars;
+const a_x = A_st_provided_x * y_f_y / .85 / X_ftg / f_c;
+const c_x = a_x / beta;
+const epsilon_t_x = (d - c_x) / c_x * .003;
+const phi_b_x = epsilon_t_x < .002 ? .65 : epsilon_t_x > .005 ? .9 : .65 + (epsilon_t_x - .002) * 250/3;
+const phiMn_x = phi_b_x * A_st_provided_x * y_f_y * (d - a_x/2);
+const M_u_x = q_u_net * (Y_ftg/12/2 - Y_col/12/2)**2 / 2 * X_ftg;
+const flexure_x_DCR = M_u_x / phiMn_x;
+//one way shear
 const phi_v = .75;
     //x-dir
-const r_w_xdir = x_A_st_provided / Y_ftg / d;
+const r_w_xdir = A_st_provided_y / Y_ftg / d;
 const phi_V_cx = phi_v * Math.max(8 * lamda_s * (r_w_xdir ** (1/3)), 1) * Math.sqrt(Math.min(f_c, 10)*1000) * d/1000;
 const phi_V_sx = shear_f_yt != "" && shear_bar_size != "" && spacing_x != "" && spacing_y != "" ? Math.min(8 * Math.sqrt(f_c*1000) * d/1000, phi_v * getArea(shear_bar_size) * Y_ftg / spacing_y * shear_f_yt * d/spacing_x / Y_ftg) : 0;
 const phi_V_nx = phi_V_cx + phi_V_sx;
 const V_ux = Math.max((X_ftg/12/2 - X_col/12/2 - d/12) * q_u_net/12, 0)
 const one_way_shear_x_DCR = V_ux / phi_V_nx ;
     //y-dir
-const r_w_ydir = y_A_st_provided / X_ftg / d;
+const r_w_ydir = A_st_provided_x / X_ftg / d;
 const phi_V_cy = phi_v * Math.max(8 * lamda_s * (r_w_ydir ** (1/3)), 1) * Math.sqrt(Math.min(f_c, 10)*1000) * d/1000;
             //there might be an error in the excel code here bc of spacing_x and spacing_y for x and y directions
 const phi_V_sy = shear_f_yt != "" && shear_bar_size != "" && spacing_x != "" && spacing_y != "" ? Math.min(8 * Math.sqrt(f_c*1000) * d/1000, phi_v * getArea(shear_bar_size) * X_ftg / spacing_x * shear_f_yt * d/spacing_x / X_ftg) : 0; 
@@ -82,7 +99,9 @@ const bearing_DCR = q_allow != "" ? q_svc_gross / q_allow : "";
 //two way shear
 
 console.log(bearing_DCR);
-console.log(one_way_shear_x_DCR);
-console.log(one_way_shear_y_DCR);
 console.log(one_way_shear_DCR)
+console.log(beta)
+console.log(flexure_x_DCR)
+console.log(flexure_y_DCR)
+
 
